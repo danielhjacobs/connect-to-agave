2efa5ced30d5321827bb51378c87e769812a2678
Daniel Jacobs <danielhunterjacobs@gmail.com>
2024-06-27 15:18:02 -0400

    Update test-cert.pl

--- test-cert.pl
+++ test-cert.pl
@@ -1,7 +1,7 @@
 use LWP::UserAgent;
 
 my $ua = LWP::UserAgent->new;
-my $response = $ua->get('https://www.google.com/');
+my $response = $ua->get('https://agave.iplant.org/');
 unless ($response->is_success) {
         die $response->status_line;
 }
